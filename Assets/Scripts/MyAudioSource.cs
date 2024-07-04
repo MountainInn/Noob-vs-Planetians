ee81ada021089e019fb7efd6d08f5dd96c9777ec
Mountain <blckclst@gmail.com>
2024-07-04 06:13:30 +0500

    Tweak sound settings to improve gunshot sounds

--- Assets/Scripts/MyAudioSource.cs
+++ Assets/Scripts/MyAudioSource.cs
@@ -3,7 +3,7 @@ using HyperCasual.Runner;
 
 public class MyAudioSource : MonoBehaviour
 {
-    [SerializeField] [Range(0, 1f)] float volume;
+    [SerializeField] [Range(0, 1f)] float volume = .75f;
     [Space]
     [SerializeField] AudioClip[] clips;
 
