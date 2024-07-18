5dee9d7884e3267f2b94973a6b10fb657be9b869
Mountain <blckclst@gmail.com>
2024-07-18 15:50:55 +0500

    Fix enemy prefabs, add new enemy animations

--- Assets/Scripts/CycleOffsetSetter.cs
+++ Assets/Scripts/CycleOffsetSetter.cs
@@ -5,9 +5,13 @@ public class CycleOffsetSetter : MonoBehaviour
     [SerializeField] Animator animator;
 
     [SerializeField] public float cycleOffset;
+    [SerializeField] public bool randomOffset;
 
     void Awake()
     {
+        if (randomOffset)
+            cycleOffset = Random.value;
+
         animator.SetFloat("cycleOffset", cycleOffset);
     }
 }
