67197297bb2e7cc29361a5748499e9dbb6bae513
Mountain <blckclst@gmail.com>
2024-08-04 10:38:21 +0500

    Add initialSize to PoolInstaller

--- Assets/Scripts/PoolInstaller.cs
+++ Assets/Scripts/PoolInstaller.cs
@@ -5,21 +5,27 @@ public class PoolInstaller : MonoInstaller
 {
     [SerializeField] Bullet prefabBullet;
     [SerializeField] FollowingTrail prefabFollowingTrail;
+    [Space]
+    [SerializeField] [Min(6)] int initialSize = 6;
 
     override public void InstallBindings()
     {
         Container
             .BindMemoryPool<Bullet, Bullet.Pool>()
+            .WithInitialSize(initialSize)
             .FromComponentInNewPrefab(prefabBullet)
             .UnderTransform(parent: null)
             .AsCached()
+            .NonLazy()
             ;
 
         Container
             .BindMemoryPool<FollowingTrail, FollowingTrail.Pool>()
+            .WithInitialSize(initialSize)
             .FromComponentInNewPrefab(prefabFollowingTrail)
             .UnderTransform(parent: null)
             .AsCached()
+            .NonLazy()
             ;
     }
 }
