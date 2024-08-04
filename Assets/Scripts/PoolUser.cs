d3eb9fa095447860d205bff54b6c8b63db956058
Mountain <blckclst@gmail.com>
2024-08-04 10:39:02 +0500

    Many changes

--- Assets/Scripts/PoolUser.cs
+++ Assets/Scripts/PoolUser.cs
@@ -3,8 +3,9 @@ using Zenject;
 
 public class PoolUser : MonoBehaviour
 {
-    static public PoolUser instance => _inst ??= FindObjectOfType<PoolUser>();
+    static public PoolUser instance => _inst;
     static PoolUser _inst;
+    PoolUser(){ _inst = this; }
 
     [Inject] public Bullet.Pool bulletPool;
     [Inject] public FollowingTrail.Pool trailPool;
